b142f0ff455e999736cf9257af45314a660f77c1
Miguel Jimenez Granizo <migueljg1997@gmail.com>
2018-10-25 20:03:20 +0200

    Added AsyncTask

--- app/src/main/java/com/example/bygra/conversordedivisa/MainActivity.java
+++ app/src/main/java/com/example/bygra/conversordedivisa/MainActivity.java
@@ -11,16 +11,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -103,39 +94,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void actualizarTasas (View view){
 
-        Toast.makeText(this,"Sincronizando",Toast.LENGTH_SHORT).show();
-
-        //Parseo XML
+        BCEConection conexion = new BCEConection();
+        conexion.execute();
         try {
-            //Conexion app-web
-            URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            //Esta mierda da error
-            Document doc = db.parse(new InputSource(url.openStream()));
-            doc.getDocumentElement().normalize();
-
-
-            //Interpretacion del XML
-            NodeList nodeList = doc.getElementsByTagName("Cube");
-            for(int i = 0; i < nodeList.getLength(); i++){
-
-                Node node = nodeList.item(i);
-
-                Element element = (Element)node;
-
-                valoresConversion[i] = Double.parseDouble(element.getAttribute("rate"));
-            }
-
-            Toast.makeText(this,"Sincronizacion finalizada",Toast.LENGTH_SHORT).show();
-
-        } catch (Exception e){
-            Toast.makeText(this,"Error al sincronizar",Toast.LENGTH_SHORT).show();
-
-
-
+            valoresConversion = conexion.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
     }
 }
